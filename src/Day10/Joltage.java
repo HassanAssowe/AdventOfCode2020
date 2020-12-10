5139ab4f29ca14b0125709130556e5ddf3a06634
HassanAssowe <44482330+HassanAssowe@users.noreply.github.com>
2020-12-10 04:33:58 -0500

    https://adventofcode.com/

--- src/Day10/Joltage.java
+++ src/Day10/Joltage.java
@@ -39,56 +39,34 @@ public class Joltage {
         System.out.println("The number of 1-jolt differences multiplied by the number of 3-jolt differences: " + oneCount * threeCount);
     }
 
-    public static Long recursion(List<Long> data, int val) {
-        int oneCount = 1;
-        int threeCount = 0;
-
-        data.add(data.get(data.size() - 1) + 3);
-        List<Long> temp = new ArrayList<>();
-
-
-        for (int i = 1; i < data.size(); i++) {
-            System.out.println("Comparing " + data.get(i) + " to " + data.get(i - 1) + " = " + (data.get(i) - data.get(i - 1)));
-            if ((data.get(i) - data.get(i - 1)) == val)
-                return data.get(i);
-        }
-
-        return null;
-    }
-
     public static void part2(List<Long> data) {
 
         data.add(data.get(data.size() - 1) + 3);
+        System.out.println("Number of combinations: " + countPart2(data, 0));
 
-        List<Long> temp = new ArrayList<>();
-        List<List<Long>> collection = new ArrayList<>();
-
-        collection = powerSet(data);
-        System.out.println("Powerset created!");
-
-        for(int i = 0; i < collection.size(); i++){
-            for(int j = 1; j < collection.get(j).size(); j++){
-                if((collection.get(i).get(j) - collection.get(i).get(j-1) != 1 || collection.get(i).get(j) - collection.get(i).get(j-1) != 2 || collection.get(i).get(j) - collection.get(i).get(j-1) != 3))
-                    collection.remove(i);
-            }
-        }
-
-        System.out.println("There are "+ collection.size()+ " Combinations");
     }
 
-    public static <T> List<List<T>> powerSet(List<T> input) {
-        List<List<T>> sets = new ArrayList<>();
-        for (T element : input) {
-            for (ListIterator<List<T>> setsIterator = sets.listIterator(); setsIterator.hasNext(); ) {
-                List<T> newSet = new ArrayList<>(setsIterator.next());
-                newSet.add(element);
-                setsIterator.add(newSet);
-            }
-            sets.add(new ArrayList<>(Arrays.asList(element)));
+    public static int countPart2(List<Long> data, int count) {
+
+        if (data.size() == 1)
+            count++;
+
+        if (data.contains(data.get(0) + 1)) {
+            //System.out.println(data.subList(data.indexOf((data.get(0) + 1)), data.size()));
+            count = countPart2(data.subList(data.indexOf((data.get(0) + 1)), data.size()), count);
         }
-        sets.add(new ArrayList<>());
-        return sets;
+        if (data.contains(data.get(0) + 2)) {
+            //System.out.println(data.subList(data.indexOf((data.get(0) + 2)), data.size()));
+            count = countPart2(data.subList(data.indexOf((data.get(0) + 2)), data.size()), count);
+        }
+        if (data.contains(data.get(0) + 3)) {
+            //System.out.println(data.subList(data.indexOf((data.get(0) + 3)), data.size()));
+            count = countPart2(data.subList(data.indexOf((data.get(0) + 3)), data.size()), count);
+        }
+        return count;
     }
+
 
 }
+
 
